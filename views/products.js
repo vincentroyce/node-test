83d0e09acd6504ac67359c9574a43dc5ca4de861
vincentroyce <vincentroyce.coe@gmail.com>
2024-11-11 14:19:15 +0800

    added more props for product model and refactored create function

--- views/products.js
+++ views/products.js
@@ -1,5 +1,7 @@
 import Product from '../models/product.js'
 import express from 'express'
+import mongoose from 'mongoose'
+import Category from '../models/category.js'
 
 const router = express.Router()
 
@@ -24,6 +26,13 @@ router.get(`/`, async (request, response) => {
 // Add a single product
 router.post(`/add-product`,  async (request, response) => {
 
+  if (!request.is('application/json')) {
+    return  response.status(415).json({
+      status: "error",
+      err_msg: "invalid mime type"
+    })
+  }
+
   let data = request.body
 
   if (!data || Object.entries(data).length === 0) {
@@ -45,19 +54,39 @@ router.post(`/add-product`,  async (request, response) => {
       status: "error",
       err_msg: "missing description key"
     })
-
   }
 
-  if (!data["price"]) {
+  if (!data["category"]) {
     return response.status(400).json({
       status: "error",
-      err_msg: "missing price key"
+      err_msg: "missing category key"
     })
   }
 
-  let {name, description, price} = data
+  if (!mongoose.isValidObjectId(data["category"])) {
+    return response.status(400).json({
+      status: "error",
+      err_msg: "invalid category id"
+    })
+  }
+
+  let category = await Category.findById(data["category"])
+
+  if (!category) {
+    return response.status(404).json({
+      status: "error",
+      err_msg: "unable to find this category"
+    })
+  }
+
+  if (!data["countInStock"]) {
+    return response.status(400).json({
+      status: "error",
+      err_msg: "missing countInStock key"
+    })
+  }
   
-  let getProduct = await Product.find({ name: name }).limit(1);
+  let getProduct = await Product.find({ name: data["name"] }).limit(1);
   
   if (getProduct.length != 0) {
     return response.status(409).json({
@@ -67,12 +96,19 @@ router.post(`/add-product`,  async (request, response) => {
   }
 
   const product = new Product({
-    name: name,
-    description:description,
-    price: price
+    name: data.name,
+    description:data.description,
+    richDescription: data.richDescription,
+    image: data.image,
+    brand: data.brand,
+    price: data.price,
+    category: data.category,
+    countInStock: data.countInStock,
+    rating: data.rating,
+    isFeatured: data.isFeatured
   })
   
-  const saveProduct = product.save()
+  const saveProduct = await product.save()
 
   if (!saveProduct) {
     return response.status(404).json({
